5f35332b326bff675a063deadb24fa2d8d89fb97
Igor Yurko <igor.yurko.mail@gmail.com>
2024-04-01 15:59:57 +0300

    test

--- node-fundamentals/index.js
+++ node-fundamentals/index.js
@@ -3,22 +3,23 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  var h = require('https');
-  var b;
-  var request = h.request({'hostname': 'https://google.com'},
-      function (response) {
-        console.log('STATUS: ' + response.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(response.headers));
-        response.setEncoding('utf8');
-        response.on('data', function (chunk) {
-          console.log('BODY: ' + chunk);
-          b = chunk;
-        });
-      });
-  request.end();
+    var username = "ui";
+    var password = "secret";
+    var auth = "Basic " + new Buffer(username + ":" + password).toString("base64");
+    var request = require('request');
+    var url = "https://smartpass.doo.dev/oauth2/oauth/check_token?token=319838fc-b137-45f4-8358-b26a3ad2e273";
+
+    request.get( {
+        url : url,
+        headers : {
+            "Authorization" : auth
+        }
+    }, function(error, response, body) {
+        console.log('body : ', body);
+    } );
 
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello world! " + b);
+  res.end("Hello world!");
 });
 
 const PORT = process.env.PORT || 3000;
